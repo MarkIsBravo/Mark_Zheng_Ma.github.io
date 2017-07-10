28120f891787a82b2fae252bc2eb2d4e46f3ee45
Mark Ma <mazheng1022@hotmail.com>
2017-07-10 14:27:14 -0400

    got some modifications

---
+++ main.js
@@ -0,0 +1,31 @@
+window.onload=function(){
+  let homeBtn=document.getElementById('homeBtn');
+  let aboutBtn=document.getElementById('aboutBtn');
+  let workBtn=document.getElementById('workBtn');
+  let contactBtn=document.getElementById('contactBtn');
+
+  //homeBtn.addEventListener('click',window.scrollTo(0, 500px))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
