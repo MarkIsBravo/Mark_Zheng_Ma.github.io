a0a037706834cf54e28996420a3ea7385477e35b
Mark Ma <mazheng1022@hotmail.com>
2017-09-03 14:05:37 -0400

    js movements

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
 window.onload=function(){
-  let homeBtn=document.getElementById('homeBtn');
-  let aboutBtn=document.getElementById('aboutBtn');
-  let workBtn=document.getElementById('workBtn');
-  let contactBtn=document.getElementById('contactBtn');
+  let $homeBtn=$('#homeBtn');
+  let $aboutBtn=$('#aboutBtn');
+  let $workBtn=$('#workBtn');
+  let $contactBtn=$('#contactBtn');
 
-  //homeBtn.addEventListener('click',window.scrollTo(0, 500px))
-
-
-
-
-
-
-
-
-
-
-
-
-
+  $homeBtn.click(function(){
+    $('html,body').animate({'scrollTop':$('html,body').scrollTop()+0});
+  })
+  $aboutBtn.click(function(){
+    $('html,body').animate({'scrollTop':$('html,body').scrollTop()+780});
+  })
+  $workBtn.click(function(){
+    $('html,body').animate({'scrollTop':$('html,body').scrollTop()+1530});
+  })
+  $contactBtn.click(function(){
+    $('html,body').animate({'scrollTop':$('html,body').scrollTop()+2500});
+  })
 
 
 
