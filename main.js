3a111cdec1af6c9f365bb27de133936e07c528ac
Mark Ma <mazheng1022@hotmail.com>
2017-10-01 20:41:25 -0400

    new paragraph animation for large screen

--- main.js
+++ main.js
@@ -5,54 +5,63 @@ window.onload = function(){
     let $contactBtn = $('#contactBtn');
 
     $homeBtn.click(function(){
-      $('html,body').animate({'scrollTop': $('.home').offset().top}, 600);
+        $('html,body').animate({'scrollTop': $('.home').offset().top}, 600);
     });
     $aboutBtn.click(function(){
-      $('html,body').animate({'scrollTop': $('.about-spacer').offset().top}, 600);
+        $('html,body').animate({'scrollTop': $('.about-spacer').offset().top}, 600);
     });
     $workBtn.click(function(){
-      $('html,body').animate({'scrollTop': $('.work-spacer').offset().top}, 600);
+        $('html,body').animate({'scrollTop': $('.work-spacer').offset().top}, 600);
     });
     $contactBtn.click(function(){
-      $('html,body').animate({'scrollTop': $('.contact-spacer').offset().top}, 600);
+        $('html,body').animate({'scrollTop': $('.contact-spacer').offset().top}, 600);
     });
 
     let $paragraph = $('.paragraphs');
     let $scroll = $('.scroll');
     
     $paragraph.click(function(){
-      let $this = $(this);
-      let p = $this.attr('id');
+        let $this = $(this);
+        let p = $this.attr('id');
         $(`#${p}`).toggleClass('selection');
-        if ($(`#${p}`).hasClass('selection')){
-          $(`.scroll-${$this.attr('id')}`).addClass('scroll-show');
-        } else if(!$(`#${p}`).hasClass('selection')){
-          $(`.scroll-${$this.attr('id')}`).removeClass('scroll-show');
-        };
+        if($(window).width() < 1200){
+          if ($(`#${p}`).hasClass('selection')){
+              $(`.scroll-${$this.attr('id')}`).addClass('scroll-show');
+          } else if(!$(`#${p}`).hasClass('selection')){
+              $(`.scroll-${$this.attr('id')}`).removeClass('scroll-show');
+          };
+        }else{
+          $(`.title-${p}`).toggleClass('title-hide');
+          $(`.single-${p}`).toggleClass('paragraph-show');
+        }
     });
 
     let $projectPic = $('.project-pic');
     let $techUsed = $('.tech-used');
 
     $projectPic.hover(function(){
-      let $this = $(this);
-      let index = $projectPic.index($this);
-      if($techUsed.hasClass(`tech-${index+1}`)){
-        $(`.tech-${index+1}`).toggleClass('show-tech');
-      }
+        let $this = $(this);
+        let index = $projectPic.index($this);
+        if($techUsed.hasClass(`tech-${index+1}`)){
+            $(`.tech-${index+1}`).toggleClass('show-tech');
+        }
     });
 
     let $skillSingle = $('.skill-single');
     let $skillName = $('.skill-name');
 
     $skillSingle.click(function(){
-      let $this = $(this);
-      $this.toggleClass('skillbackground-show');
+        let $this = $(this);
+        if($(window).width() < 1200){
+          $this.toggleClass('skillbackground-show');
+        }
     })
 
     $skillName.click(function(){
-      let $this = $(this);
-      $this.toggleClass('skillname-show');
+        let $this = $(this);
+        if($(window).width() < 1200){
+          $this.toggleClass('skillname-show');
+        }
     })
 
 
